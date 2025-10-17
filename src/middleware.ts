ef30db6f9df5f3190675fa217cf082bfa3aec81c
abdoemadselim <abdoemadselim11@gmail.com>
2025-10-17 21:23:38 +0300

    feat(auth): add protected routes with next middleware
    - send a request to backend to verify the cookie
    - in case it's not valid, redirect user to login page
    - otherwise, continue the normal flow

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server"
+
+function isProtectedRoute(request: NextRequest) {
+    return request.nextUrl.pathname.startsWith("/dashboard")
+}
+
+export default async function middleware(request: NextRequest) {
+    if (isProtectedRoute(request)) {
+        try {
+            const res = await fetch(`${process.env.API_URL}/auth/me`, {
+                headers: {
+                    cookie: request.headers.get("cookie") || "",
+                },
+                credentials: "include",
+            })
+
+            const data = await res.json()
+
+            // If not authenticated, redirect to login
+            if (!data?.data?.user) {
+                const loginUrl = new URL("/auth/login", request.url)
+                return NextResponse.redirect(loginUrl)
+            }
+        } catch (err) {
+            console.error("Auth check failed:", err)
+            const loginUrl = new URL("/auth/login", request.url)
+            return NextResponse.redirect(loginUrl)
+        }
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/dashboard/:path*',
+    ],
+}
